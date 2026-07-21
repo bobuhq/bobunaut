7a2edb55aeea59e8e09b9564cf37bd0e79b69506
bobuhq <denyzcyber@gmail.com>
2026-07-21 20:23:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,8 @@
-import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; import tailwindcss from "@tailwindcss/vite"; export default defineConfig({plugins:[react(),tailwindcss()]});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/bobunaut/",
+  plugins: [react(), tailwindcss()],
+});
