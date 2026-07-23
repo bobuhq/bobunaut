4ff4bb12f21f5c1ea3b6b2b238f8f7d2591dac23
Mehmet Ali Topal <dr_mtopal@hotmail.com>
2026-07-23 11:33:56 +0300

    Remove BrowserRouter basename for custom domain

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import { App } from "./app/App";
 import "./styles/global.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
-    <BrowserRouter basename="/bobunaut">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
